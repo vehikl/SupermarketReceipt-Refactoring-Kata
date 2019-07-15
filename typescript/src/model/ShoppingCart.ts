6eedbd1ec37f003410429910ed2482a9d85e26fa
Vehikl Team <go@vehikl.com>
2019-07-15 10:44:40 -0400

    remove unused methods

--- typescript/src/model/ShoppingCart.ts
+++ typescript/src/model/ShoppingCart.ts
@@ -62,23 +62,6 @@ export class ShoppingCart {
         if (allSpecialOffers && allSpecialOffers.length) {
             return this.calcDiscountUsingNewStructure(allSpecialOffers, receipt);
         }
-
-        this.calcDiscountUsingTheOldWay(offers, catalog, receipt);
-    }
-
-    private calcDiscountUsingTheOldWay(offers: OffersByProduct, catalog: SupermarketCatalog, receipt: Receipt) {
-        for (const productName in this.productQuantities()) {
-            const productQuantity = this._productQuantities[productName];
-            const product = productQuantity.product;
-            if (offers[productName]) {
-                const offer: Offer = offers[productName];
-                const unitPrice: number = catalog.getUnitPrice(product);
-                const offerType = offer.offerType;
-                let discount = this.getDiscountForOffer(productName, offerType, offer, unitPrice, product);
-                if (discount != null)
-                    receipt.addDiscount(discount);
-            }
-        }
     }
 
     private calcDiscountUsingNewStructure(allSpecialOffers: OfferInterface[], receipt: Receipt) {
@@ -88,52 +71,5 @@ export class ShoppingCart {
             }
         });
         return;
-    }
-
-    private getDiscountForOffer(productName: string, offerType: SpecialOfferType, offer: Offer, unitPrice: number, product: Product) {
-        const quantity: number = this._productQuantities[productName].quantity;
-
-        let discount: Discount | null = null;
-
-        discount = this.getTwoForAmountDiscount(offerType, offer, unitPrice, discount, product);
-        discount = this.threeForTwoDiscount(offerType, unitPrice, discount, product);
-        discount = this.percentageDiscount(offerType, discount, product, offer, unitPrice);
-        discount = this.fiveForAmountDiscount(offerType, unitPrice, offer, discount, product);
-        return discount;
-    }
-
-    private fiveForAmountDiscount(offerType: SpecialOfferType, unitPrice: number, offer: Offer, discount: Discount | null, product: Product) {
-        const fiveForAmount: FiveForAmountOffer = new FiveForAmountOffer(product, unitPrice, offer.argument);
-
-        if (offerType == SpecialOfferType.FiveForAmount && fiveForAmount.applies(this)) {
-            return fiveForAmount.getDiscount(this);
-        }
-        return discount;
-    }
-
-    private percentageDiscount(offerType: SpecialOfferType, discount: Discount | null, product: Product, offer: Offer, unitPrice: number) {
-        const percentageDiscount: PercentageDiscountOffer = new PercentageDiscountOffer(product, unitPrice, offer.argument);
-        if (offerType == SpecialOfferType.TenPercentDiscount && percentageDiscount.applies(this)) {
-            return percentageDiscount.getDiscount(this);
-        }
-        return discount;
-    }
-
-    private threeForTwoDiscount(offerType: SpecialOfferType, unitPrice: number, discount: Discount | null, product: Product) {
-        const threeForTwo: ThreeForTwoOffer = new ThreeForTwoOffer(product, unitPrice);
-
-        if (offerType == SpecialOfferType.ThreeForTwo && threeForTwo.applies(this)) {
-            return threeForTwo.getDiscount(this);
-        }
-        return discount;
-    }
-
-    private getTwoForAmountDiscount(offerType: SpecialOfferType, offer: Offer, unitPrice: number, discount: Discount | null, product: Product) {
-        const twoForAmount: TwoForAmountOffer = new TwoForAmountOffer(product, unitPrice, offer.argument);
-
-        if (offerType == SpecialOfferType.TwoForAmount && twoForAmount.applies(this)) {
-            return twoForAmount.getDiscount(this);
-        }
-        return discount;
     }
 }
